dbab55128c8e8449a2036c14cf5edb2c082fdf6d
Sumit Lodhi <sl8421866@gmail.com>
2026-03-14 12:54:57 +0530

    feat: add epaper routes to the Express app for API integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 import newsRoutes from "./routes/newsRoutes.js";
+import epaperRoutes from "./routes/epaperRoutes.js";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/news", newsRoutes);
+app.use("/api/epaper", epaperRoutes);
 
 export default app;
